c78c6c74cc9dded23c4608e2163ab096832be698
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-04 11:40:31 -0700

    bind function.

--- interview.js
+++ interview.js
@@ -1,9 +1,11 @@
-//Write a function that would allow you to do this.
-//multiply(5)(6);
-
-function multiply(num1){
-    return function(num2){
-        return (num1)*(num2)
-    }
-}
-console.log(multiply(5)(6))
+//When would you use the bind function?
+function fullName() {
+    return "Hello, this is " + this.first + " " + this.last;
+  }
+  
+  console.log(fullName()); // => Hello this is undefined undefined
+  
+  // create a person object and pass its values to the fullName function
+  var person = {first: "Foo", last: "Bar"};
+  console.log(fullName.bind(person)()); // => Hello this is Foo Bar
+  
