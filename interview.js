f0527bccd2a93ea5a8f3207efeeaebd6be838d43
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-04 11:52:46 -0700

    using prototype to define a function method.

--- interview.js
+++ interview.js
@@ -1,11 +1,15 @@
-//When would you use the bind function?
-function fullName() {
-    return "Hello, this is " + this.first + " " + this.last;
+//How would you add your own method to the Array object so the following code would work?
+// var arr = [1, 2, 3, 4, 5];
+// var avg = arr.average();
+// console.log(avg);
+
+Array.prototype.average = function() {
+    // calculate sum
+    var sum = this.reduce(function(prev, cur) { return prev + cur; });
+    // return sum divided by number of elements
+    return sum / this.length;
   }
   
-  console.log(fullName()); // => Hello this is undefined undefined
-  
-  // create a person object and pass its values to the fullName function
-  var person = {first: "Foo", last: "Bar"};
-  console.log(fullName.bind(person)()); // => Hello this is Foo Bar
-  
+  var arr = [1, 2, 3, 4, 5];
+  var avg = arr.average();
+  console.log(avg); // => 3
