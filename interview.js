4e942d095ec6c958bfb76783fb5ea0486d24ca17
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-04 11:37:18 -0700

    function returns another function.

--- interview.js
+++ interview.js
@@ -1,13 +1,9 @@
-// What will the following code output?
+//Write a function that would allow you to do this.
+//multiply(5)(6);
 
-(function() {
-    var a = b = 5;
-  })();
-  
-  console.log(b); //5
-
-// The code above will output 5 even though it seems as if the variable was declared within a function and can't be accessed outside of it. This is because
-//var a = b = 5; is interpreted the following way:
-    // var a = b;
-    // b = 5;
-//But b is not declared anywhere in the function with var so it is set equal to 5 in the global scope.
+function multiply(num1){
+    return function(num2){
+        return (num1)*(num2)
+    }
+}
+console.log(multiply(5)(6))
