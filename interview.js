7a8e89705b8deac6fe623b56cfdcb890ee5c3ac7
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-07 20:24:15 -0700

    palindrome check

--- interview.js
+++ interview.js
@@ -1,10 +1,45 @@
-//String reversal
+//Palindrome
+let sampleStr = "racecar";
 
-let sampleStr = 'Reverse Me'
-const sampleArr = sampleStr.split('')
-const reversedArr = sampleArr.reverse().join('')
+//======== Method 1 ============//
 
+const isPalindrom = function() {
+  let newStr = sampleStr
+    .split("")
+    .reverse()
+    .join("");
 
-console.log(reversedArr)
+  if (newStr === sampleStr) {
+    return true;
+  } else {
+    return false;
+  }
+};
+//=================================//
 
+//=============== Method 2 ========//
 
+// function isPalindrom() {
+//   for (let i = 0; i < sampleStr.length; i++) {
+//     if (sampleStr[i] !== sampleStr[sampleStr.length - (i + 1)]) {
+//       return false;
+//     } else return true;
+//   }
+// }
+//==================================//
+
+//=============== Method 3 ========//
+
+// function isPalindrom(){
+//   const sampleArr = sampleStr.split('')
+//   console.log(sampleArr)
+//   for (let i = 0; i< sampleArr.length; i++){
+//     if (sampleArr[i] !== sampleArr[sampleArr.length - (i + 1)]){
+//      return false;
+//     }
+//     else {
+//       return true;
+//     }
+//   }
+// }
+console.log(isPalindrom(sampleStr));
