089e0eacdd7c0b7c522a15806b9832d14b02dd42
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-07 19:22:15 -0700

    string reversal

--- interview.js
+++ interview.js
@@ -1,21 +1,10 @@
-var data = [
-  {name: 'daniel', age: 45},
-  {name: 'john', age: 34},
-  {name: 'robert', age: null},
-  {name: 'jen', age: undefined},
-  {name: null, age: undefined}
-];
+//String reversal
+
+let sampleStr = 'Reverse Me'
+const sampleArr = sampleStr.split('')
+const reversedArr = sampleArr.reverse().join('')
 
 
-
-const filteredData = data.filter(function(el){
-  if(el.name != undefined && el.age != undefined){
-    return true;
-  }
-  else {
-    return false};
-})
-
-console.log(filteredData)
+console.log(reversedArr)
 
 
