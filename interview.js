a890d95f53ead13c108a769db627918645b029a7
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-08 18:35:10 -0700

    max stock profit

--- interview.js
+++ interview.js
@@ -1,31 +1,16 @@
-
-//   First, I wanna know how much money I could have made yesterday if I’d been trading Apple stocks all day.
-//
-//   So I grabbed Apple’s stock prices from yesterday and put them in an array called stockPrices, where:
-//
+// First, I wanna know how much money I could have made yesterday if I’d been trading Apple stocks all day.
+// So I grabbed Apple’s stock prices from yesterday and put them in an array called stockPrices, where:
 // The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
-//   The values are the price (in US dollars) of one share of Apple stock at that time.
-//   So if the stock cost $500 at 10:30am, that means stockPrices[60] = 500.
-//
+// The values are the price (in US dollars) of one share of Apple stock at that time.
+// So if the stock cost $500 at 10:30am, that means stockPrices[60] = 500.
 // Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
-//
-//   For example:
-//
+// Returns 6 (buying for $5 and selling for $11)
 
-//
-
-
-
-
-
-// // Returns 6 (buying for $5 and selling for $11)
-
-
-const stockPrices = [10, 7,5,8,11,9];
+const stockPrices = [10, 7, 5, 8, 11, 9];
 
 function getMaxProfit(stockPrices) {
   if (stockPrices.length < 2) {
-    throw new Error('Getting a profit requires at least 2 prices');
+    throw new Error("Getting a profit requires at least 2 prices");
   }
 
   // We'll greedily update minPrice and maxProfit, so we initialize
@@ -42,11 +27,9 @@ function getMaxProfit(stockPrices) {
   for (let i = 1; i < stockPrices.length; i++) {
     const currentPrice = stockPrices[i];
 
-   
     // See what our profit would be if we bought at the
     // min price and sold at the current price
     const potentialProfit = currentPrice - minPrice;
-    
 
     // Update maxProfit if we can do better
     maxProfit = Math.max(maxProfit, potentialProfit);
@@ -59,19 +42,6 @@ function getMaxProfit(stockPrices) {
   return maxProfit;
 }
 
-console.log(getMaxProfit(stockPrices))
-
-
-
-
-
-
-
-
-/*
-
-SOLUTION
-
+console.log(getMaxProfit(stockPrices));
 
 // No “shorting”—you need to buy before you can sell. Also, you can’t buy and sell in the same time step—at least 1 minute has to pass.
-*/
