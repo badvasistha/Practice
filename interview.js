a9cc4d76ebcbd0cd3bf6f8a9b8de7de1daac56a5
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-08 18:20:34 -0700

    average

--- interview.js
+++ interview.js
@@ -1,9 +1,22 @@
-// Reverse Words
-
-var phrase = 'I love Javascript';
-function reversePhrase(){
-  var reverseP = phrase.split('').reverse().join('');
-  console.log(reverseP)
+// Return an Average
+//===========Method1===============
+const stat1 = [1, 2, 3, 4, 4, 5, 5];
+function average(){
+  const sum = stat1.reduce(function(prev, curr){
+    return (prev + curr);  
+   })
+  return sum/stat1.length
 }
+console.log(average(stat1))
 
-reversePhrase(phrase)
+
+//=========Method2===========================
+// Array.prototype.average = function(){
+//   const sum = this.reduce(function(prev, curr){
+//     return prev + curr
+//   })
+//   return sum/this.length;
+// }
+// const avg = stat1.average();
+// console.log(avg)
+
