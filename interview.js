c3b4f57f007501cb388b35500e2f07f0ab2ddf07
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-10 09:59:58 -0700

    middle element of a linked list

--- interview.js
+++ interview.js
@@ -1,12 +1,41 @@
-// Fibonacci Sequence
+//Finding the middle element of a linked list
+//A linked list is a data structure which consists of a group of nodes where each node may point to some other node to form a sequence.
+//Our nodes will have two fields:
+//(1) a "data" field which will store our data (string, number, etc.)
+//(2) a "next" field which will be a reference to some other node
 
-var fib = []; // Initialize array!
-fib[0] = 0,
-fib[1] = 1
-
-for (let i = 2; i< 10; i++){
-  fib[i] = fib[i -2]+ fib[i - 1];
-  console.log(fib[i])
+function Node(data, next) {
+  this.data = data;
+  this.next = next;
 }
 
+// setup some nodes and connect them to each other
+// the linked list looks like:
+// (head) n5 -> n4 -> n3 -> n2 -> n1 -> null
+var n1 = new Node("Hello", null);
+var n2 = new Node("21", n1);
+var n3 = new Node("Green", n2);
+var n4 = new Node("Blue", n3);
+var n5 = new Node("Daniel", n4);
 
+// assign a node to the head which functions
+// as the entry into our linked list
+var head = n5;
+
+// setup pointers to both start
+// at the head of the linked list
+var fastPointer = head;
+var slowPointer = head;
+
+// loop through the linked list
+// when fastPointer reaches the end of the list
+// then slowPointer will be at the middle node
+while (fastPointer.next !== null && fastPointer.next.next !== null) {
+  fastPointer = fastPointer.next.next;
+  slowPointer = slowPointer.next;
+}
+
+// slowPointer is now at the middle node in the linked list
+console.log(slowPointer.data);
+
+//Running time: O(n)
